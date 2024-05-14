5baa50e4d29edafd03855ac23daa11e53aeb69ab
bhavyamehta353 <85569570+bhavyamehta353@users.noreply.github.com>
2024-05-14 23:35:26 +0530

    Updated SJF.cpp
    The code will now handle all cases including the case where only few processes have arrived after the first process has completed. Previously, it was was sorting based on burst time without checking the arrival of processes

--- ProcessScheduling/SJF.cpp
+++ ProcessScheduling/SJF.cpp
@@ -8,35 +8,50 @@ bool compareAT(Process a, Process b)
 {
     return a.at < b.at;
 }
-
-bool compareBT(Process a, Process b)
+bool compareCT(Process a, Process b)
 {
-    return a.bt < b.bt;
+    return a.ct < b.ct;
 }
 void SJF(struct Process P[] , int n)
 {            
-    sort(P + 1, P + n, compareBT);
-    for(int i = 0 ; i < n ;i++)
+    int time = 0;
+    int completed = 0;
+    int visited[n];
+    fill(visited ,visited + n , 0);
+    while(completed < n)
     {
-        if(i == 0)
-            P[i].ct = P[i].at + P[i].bt;
-        else
-            P[i].ct = P[i-1].ct + P[i].bt;
+        int min_index = -1; //for min burst time
+        int min = INT_MAX;
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(P[i].at <= time && !visited[i] && P[i].bt < min) // finds the index of min burst time among processes that have arrived
+            {
+                min = P[i].bt;
+                min_index = i;
+            }
+        }
+        if(min_index == -1) //if no processes have arrived
+        {
+            time++;
+            continue;
+        }
+        // else
+        visited[min_index] = 1; 
+        P[min_index].ct = time + P[min_index].bt; // since it is non preemptive
+        time += P[min_index].bt;
+        completed++;
     }
+    sort(P, P + n , compareCT);
     cout << endl << "Gantt Chart" << endl << "0" << " ";
     for(int i = 0 ; i < n ; i++)
         cout << P[i].ct << " ";
     
     int TT[n] , WT[n];
+    float AWT = 0.0 , ATT = 0.0; 
     for(int i = 0 ; i < n ; i++)
     {
         TT[i] = P[i].ct - P[i].at;
         WT[i] = TT[i] - P[i].bt;
-    }
-
-    float AWT = 0.0 , ATT = 0.0; 
-    for(int i = 0 ; i < n ; i++)
-    {
         AWT += WT[i];
         ATT += TT[i];
     }
